93b31b02781391f21342cd2de72e934116788294
Lucas Tamura <lucasestvez007@gmail.com>
2024-04-23 18:39:51 -0300

    first commit

---
+++ assets/js/fullImg.js
@@ -0,0 +1,13 @@
+const fullImgBox = document.getElementById("fullImgBox");
+const fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic) {
+
+    fullImgBox.style.display = 'flex';
+    fullImg.src = pic;
+}
+
+function closeFullImg() {
+    
+    fullImgBox.style.display = 'none';
+}
